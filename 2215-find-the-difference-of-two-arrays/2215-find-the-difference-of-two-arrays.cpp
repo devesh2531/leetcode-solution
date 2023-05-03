0e61b86526a6bbdf06ce8752f1bef4864fd837de
Devesh Charan <devesh.charan2020@vitbhopal.ac.in>
2023-05-03 22:43:01 +0530

    Time: 55 ms (76.30%), Space: 31.6 MB (67.27%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    // Returns the elements in the first arg nums1 that don't exist in the second arg nums2.
-    vector<int> getElementsOnlyInFirstList(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> onlyInNums1;
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
         
-        // Iterate over each element in the list nums1.
-        for (int num : nums1) {
-            bool existInNums2 = false;
-            // Check if num is present in the second arg nums2.
-            for (int x : nums2) {
-                if (x == num) {
-                    existInNums2 = true;
-                    break;
-                }
-            }
-            
-            if (!existInNums2) {
-                onlyInNums1.insert(num);
+        vector<int> distinct_nums1, distinct_nums2;
+        for (int num : set1) {
+            if (set2.count(num) == 0) {
+                distinct_nums1.push_back(num);
             }
         }
-        
-        // Convert to vector.
-        return vector<int> (onlyInNums1.begin(), onlyInNums1.end());
-    }
-    
-    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        return {getElementsOnlyInFirstList(nums1, nums2), getElementsOnlyInFirstList(nums2, nums1)};
+
+        for (int num : set2) {
+            if (set1.count(num) == 0) {
+                distinct_nums2.push_back(num);
+            }
+        }
+
+        return {distinct_nums1, distinct_nums2};
     }
 };

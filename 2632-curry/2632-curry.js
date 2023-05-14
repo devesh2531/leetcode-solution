30f2ded89c47d699b8af3def176df32643030a9f
Devesh Charan <devesh.charan2020@vitbhopal.ac.in>
2023-05-14 15:31:59 +0530

    Time: 73 ms (43.08%), Space: 48.4 MB (45.78%) - LeetHub

--- 2632-curry/2632-curry.js
+++ 2632-curry/2632-curry.js
@@ -1,9 +1,20 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
 var curry = function(fn) {
-   return function curried(...args) {
-      if(args.length >= fn.length) {
-         return fn(...args);
+ return function curried(...args) {
+    if (args.length < fn.length) {
+      return function(...args2) {
+        return curried(...args, ...args2);
       }
-
-      return (...nextArgs) => curried(...args, ...nextArgs);
-   };
+    }
+    return fn(...args);
+  };
 };
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
